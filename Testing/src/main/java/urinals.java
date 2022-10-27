18b623203a994cad14b9f4f1439516c4192f9391
jpate108 <jpate108@asu.edu>
2022-10-27 15:29:24 -0700

    committing the skeleton of the assignment

---
+++ Testing/src/main/java/urinals.java
@@ -0,0 +1,5 @@
+public class urinals {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Testing/src/main/java/validurinals.java
@@ -0,0 +1,22 @@
+public class validurinals {
+
+    public void getTheString(){
+        //to get the string from the user
+    }
+
+    public void openTheFile(){
+        //to read the input from the file that is given by the user
+    }
+
+    public boolean checkForValidString( String str ) {  // checks to see if valid string
+        System.out.println ("Not yet implemented");
+        return true;
+    }
+
+    public int countValidUrinals(){
+        //to count the number of the urinals available to use
+        return 0;
+    }
+
+
+}
