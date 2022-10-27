4d0bfb66d5a1c00332b4127411ee50e80b79f2fa
jpate108 <jpate108@asu.edu>
2022-10-27 16:50:06 -0700

    Commiting the some of the implemented methods

--- Testing/src/main/java/urinals.java
+++ Testing/src/main/java/urinals.java
@@ -1,5 +1,9 @@
+/*
+author : Jay Patel(jpate108@gmail.com
+ */
+
 public class urinals {
     public static void main(String[] args){
-
+            validurinals.pickTheChoice();
     }
 }
--- Testing/src/main/java/validurinals.java
+++ Testing/src/main/java/validurinals.java
@@ -1,19 +1,46 @@
-public class validurinals {
+import java.util.Scanner;
 
-    public void getTheString(){
+public class validurinals {
+    static Scanner s = new Scanner(System.in);
+    public static void pickTheChoice(){
+        System.out.println("Enter how you want to give the strings");
+        System.out.println("1 for input from the keyboard");
+        System.out.println("2 for input from the file");
+
+        int a = s.nextInt();
+
+        if(a==1){
+            getTheString();
+        }
+        else{
+            openTheFile();
+        }
+    }
+    public static void getTheString(){
         //to get the string from the user
+        System.out.println("Enter the string for urinals");
+
+        String str= s.next();
+        if(!checkForValidString(str)){
+            System.out.println("You have entered the wrong string for validation please try again");
+        }
+        else {
+            countValidUrinals(str);
+        }
+
+
     }
 
-    public void openTheFile(){
+    public static void openTheFile(){
         //to read the input from the file that is given by the user
     }
 
-    public boolean checkForValidString( String str ) {  // checks to see if valid string
+    public static boolean checkForValidString( String str ) {  // checks to see if valid string
         System.out.println ("Not yet implemented");
         return true;
     }
 
-    public int countValidUrinals(){
+    public static int countValidUrinals(String str){
         //to count the number of the urinals available to use
         return 0;
     }
