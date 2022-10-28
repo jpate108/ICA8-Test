3c490da6004ce267ce8203f726569e93912dc267
jpate108 <jpate108@asu.edu>
2022-10-27 20:07:38 -0700

    first test method failing one

--- Testing/src/test/java/urinalsTest.java
+++ Testing/src/test/java/urinalsTest.java
@@ -6,8 +6,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 class urinalsTest {
+    validurinals uri=new validurinals();
 
+    @BeforeEach
+    void setUpofthemethod() {
+    }
+
+    @AfterEach
+    void tearDownofthemethod() {
+    }
     @org.junit.jupiter.api.Test
     void main() {
+    }
+
+    @Test
+    void checkForValidString(){
+
+        Assertions.assertEquals(true,uri.checkForValidString(new String("91018")));
+        System.out.println("====== Jay Patel == TEST ONE EXECUTED =======");
     }
 }
