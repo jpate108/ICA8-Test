5a424ce96e813c9cadf04da96c44cbe0393c4688
jpate108 <jpate108@asu.edu>
2022-10-27 18:10:48 -0700

    Committing the files

--- Testing/src/main/java/urinals.java
+++ Testing/src/main/java/urinals.java
@@ -2,8 +2,10 @@
 author : Jay Patel(jpate108@gmail.com
  */
 
+import java.io.IOException;
+
 public class urinals {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
             validurinals.pickTheChoice();
     }
 }
--- Testing/src/main/java/validurinals.java
+++ Testing/src/main/java/validurinals.java
@@ -1,8 +1,10 @@
+import java.io.*;
 import java.util.Scanner;
+
 
 public class validurinals {
     static Scanner s = new Scanner(System.in);
-    public static void pickTheChoice(){
+    public static void pickTheChoice() throws IOException {
         System.out.println("Enter how you want to give the strings");
         System.out.println("1 for input from the keyboard");
         System.out.println("2 for input from the file");
@@ -21,27 +23,52 @@ public class validurinals {
         System.out.println("Enter the string for urinals");
 
         String str= s.next();
-        if(!checkForValidString(str)){
-            System.out.println("You have entered the wrong string for validation please try again");
+        if(checkForValidString(str)){
+
+            System.out.println("djancjks");
+            countValidUrinals(str);
+
         }
         else {
-            countValidUrinals(str);
+            System.out.println("-1");
         }
 
 
     }
 
-    public static void openTheFile(){
-        //to read the input from the file that is given by the user
+    public static void openTheFile() throws IOException {
+        File file = new File("testing\\src\\main\\resources\\urinal.dat");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String b;
+        while ((b = br.readLine()) != null){
+            if(checkForValidString(b)){
+                System.out.println("sasasasa");
+                countValidUrinals(b);
+            }
+            else {
+
+                System.out.println("-1");
+            }
+        }
     }
 
     public static boolean checkForValidString( String str ) {  // checks to see if valid string
-        System.out.println ("Not yet implemented");
+        //System.out.println ("Not yet implemented");
+        char[] c = str.toCharArray();
+        int i=0,j=0;
+        for(i=0;i<c.length;i++)
+        {
+            if((c[i]=='1' && i+1<c.length && c[i]==c[i+1]) || (c[i]!='1' && c[i]!='0') )
+            {
+                return false;
+            }
+        }
         return true;
     }
 
     public static int countValidUrinals(String str){
         //to count the number of the urinals available to use
+
         return 0;
     }
 
