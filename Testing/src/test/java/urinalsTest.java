ca776c678a7b91d6d929e25c7df00068fa3f7c0d
jpate108 <jpate108@asu.edu>
2022-10-27 20:14:36 -0700

    adding bunch of test case to test the method

--- Testing/src/test/java/urinalsTest.java
+++ Testing/src/test/java/urinalsTest.java
@@ -23,6 +23,14 @@ class urinalsTest {
     void checkForValidString(){
 
         Assertions.assertEquals(false,uri.checkForValidString(new String("91018")));
+        Assertions.assertEquals(false,uri.checkForValidString(new String("127%881,0")));
+        Assertions.assertEquals(true,uri.checkForValidString(new String("1000100101")));
+        Assertions.assertEquals(true,uri.checkForValidString(new String("0100010")));
+        Assertions.assertEquals(false,uri.checkForValidString(new String("098")));
+        Assertions.assertEquals(true,uri.checkForValidString(new String("10100")));
+        Assertions.assertEquals(true,uri.checkForValidString(new String("00000100010001")));
+        Assertions.assertEquals(false,uri.checkForValidString(new String("110")));
+        Assertions.assertEquals(false,uri.checkForValidString(new String("11111111111")));
         System.out.println("====== Jay Patel == TEST ONE EXECUTED =======");
     }
 }
