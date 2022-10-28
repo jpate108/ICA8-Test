f059a7798ef9bb60c47be7edf5c4c3eee0d4ff03
jpate108 <jpate108@asu.edu>
2022-10-27 22:57:39 -0700

    committing the final version of the code

--- Testing/src/main/java/urinals.java
+++ Testing/src/main/java/urinals.java
@@ -3,7 +3,7 @@ author : Jay Patel(jpate108@gmail.com
  */
 
 import java.io.IOException;
-
+//creating the main method for assignment
 public class urinals {
     public static void main(String[] args) throws IOException {
             validurinals.pickTheChoice();
--- Testing/src/main/java/validurinals.java
+++ Testing/src/main/java/validurinals.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class validurinals{
     static Scanner s = new Scanner(System.in);
+    /*
+    creating the choice method to chose from keyboard or file
+     */
     public static void pickTheChoice() throws IOException {
         System.out.println("Enter how you want to give the strings");
         System.out.println("1 for input from the keyboard");
         System.out.println("2 for input from the file");
 
         int a = s.nextInt();
-
+        /*
+        calling the appropriate methods to run
+         */
         if(a==1){
             getTheString();
         }
@@ -18,12 +23,15 @@ public class validurinals{
             openTheFile();
         }
     }
+    /*
+    creating the method to the string from the keyboard
+     */
     public static void getTheString(){
         //to get the string from the user
         System.out.println("Enter the string for urinals");
         System.out.println("Enter -1 if you have enter all the required input");
         String str=" ";
-        while(str!="-1") {
+        while(!str.equals("-1")) {
             str = s.next();
             int a = -1;
             if (!str.equals("-1")){
@@ -39,27 +47,38 @@ public class validurinals{
         }
 
     }
+    /*
+    creating the file methods to get input from the files
+     */
 
     public static void openTheFile() throws IOException {
-        String pn=setNameofthePath();
+
         File file = new File("testing\\src\\main\\resources\\urinal.dat");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String b;
         int x;
-        FileWriter w = new FileWriter(pn, true);
-        while ((b = br.readLine()) != null){
-            x = -1;
-            if(checkForValidString(b)){
-                x = countValidUrinals(b);
+        if ((b = br.readLine()) == null) {
+            System.out.println("File is empty");
+
+        } else {
+            String pn = setNameofthePath();
+            FileWriter w = new FileWriter(pn, true);
+            while ((b = br.readLine()) != null) {
+                x = -1;
+                if (checkForValidString(b)) {
+                    x = countValidUrinals(b);
+                }
+                System.out.println(x);
+                w.write(x + "\n");
+
             }
-            System.out.println(x);
-            w.write(x + "\n" );
-
+            w.close();
+            System.out.println("file has been created");
         }
-        w.close();
-        System.out.println("file has been created");
     }
-
+    /*
+    creating the method to write in the file to create the output file according to instructions
+     */
     public static String setNameofthePath() throws IOException{
         int z=0;
         String pn="testing\\src\\main\\resources\\rule.txt";
@@ -68,7 +87,6 @@ public class validurinals{
             File o = new File(pn);
             if(o.createNewFile()){
                 System.out.println("Creating the file" + o.getName());
-                z=0;
                 break;
             }
             else{
@@ -78,6 +96,9 @@ public class validurinals{
         }
         return pn;
     }
+    /*
+    method to check that string is valid or not
+     */
 
     public static boolean checkForValidString( String str ) {  // checks to see if valid string
         //System.out.println ("Not yet implemented");
@@ -92,7 +113,10 @@ public class validurinals{
         }
         return true;
     }
-
+    /*
+    creating the method to count the number of the urinals are available to use
+    and then returning to the function for final output
+     */
     public static int countValidUrinals( String str ){
         //to count the number of the urinals available to use
 
@@ -105,21 +129,26 @@ public class validurinals{
         for(int i = 0; i < l ; i++ ){
             if( arr[i] == '0' ){
                 if( i == 0 ){
-                    if ( arr [i + 1] == '0' ) {
+                    if(l == 1){
                         c += 1;
-                        arr[i] = 1;
+                        arr[i] = '1';
+                        break;
+                    }
+                    else if ( arr [i + 1] == '0' ) {
+                        c += 1;
+                        arr[i] = '1';
                     }
                 }
                 else if( i == (l-1) ){
                     if ( arr[i - 1] == '0' ) {
                         c += 1;
-                        arr[i] = 1;
+                        arr[i] = '1';
                     }
                 }
                 else{
                     if( arr[i+1]=='0' && arr[i-1]=='0' ){
                         c += 1;
-                        arr[i] = 1;
+                        arr[i] = '1';
                     }
                 }
             }
